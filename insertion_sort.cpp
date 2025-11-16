a31577a3928e6e6d91105daf22d1874f2781c806
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-11-16 20:57:36 +0600

    insertion-sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 7;
+    int arr[n] = {4, 6, 7, 8, 0, 4, 2};
+
+    for (int i = 1; i < n; i++) {
+        int key = i;
+        int j = i - 1;
+
+        for (j = i-1; (j >= 0) && (key < arr[j]); j--) {
+            arr[j + 1] = arr[j];
+        }
+        // while (key < arr[j] && j >= 0) {
+        //     arr[j + 1] = arr[j];
+        //     j--;
+        // }
+        arr[j + 1] = key;
+    }
+
+    for (auto i : arr) {
+        cout << i << " ";
+    } 
+    cout << endl;
+}
